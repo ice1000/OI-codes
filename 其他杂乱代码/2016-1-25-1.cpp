971cce0b5a4f0fc7ebe194a6aab8ad930b28e7ba
ice1000 <951394653@qq.com>
2016-01-25 09:58:08 +0800

    Create 2016-1-25-1.cpp

---
+++ 其他杂乱代码/2016-1-25-1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+class line {
+    public:
+        line* next;
+        int end;
+        int value;
+        line(){};
+        line(int e,int v){end = e;value = v;}
+};
+
+int main(int argc,char** argv){
+    
+    return 0;
+}
