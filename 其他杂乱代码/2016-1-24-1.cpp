2122443346c9ac25a0cc20e76bd61bda9bcffb63
千里冰封 <zhangyinsen@126.com>
2016-01-26 08:30:13 +0800

    每天写点码，对身体好

--- 其他杂乱代码/2016-1-24-1.cpp
+++ 其他杂乱代码/2016-1-24-1.cpp
@@ -4,6 +4,17 @@ const int MAX = 300;
 
 int f[MAX];
 
+int find(int x){
+	if(x == f[x]) return x;
+	else return f[x] = find(f[x]);
+}
+
+void unionSet(int x, int y){
+	int xx = find(x);
+	int yy = find(y);
+	f[yy] = xx;
+}
+
 int main(int argc,char** argv){
 	return 0;
 }
--- 其他杂乱代码/2016-1-25-1.cpp
+++ 其他杂乱代码/2016-1-25-1.cpp
@@ -1,4 +1,8 @@
 #include<stdio.h>
+#include<vector>
+#include<queue>
+
+using namespace std;
 
 class line {
     public:
@@ -10,6 +14,13 @@ class line {
 };
 
 int main(int argc,char** argv){
-    
+
+    queue<int> q;
+    for(int i = 0; i < n; i++)
+        if(deg[i] == 0)
+            q.push(deg[i]);
+
+
+
     return 0;
 }
