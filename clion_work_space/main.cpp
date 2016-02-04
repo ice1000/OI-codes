aac502910576cb04b132f4202d1b053a8b034adb
千里冰封 <zhangyinsen@126.com>
2016-02-04 14:43:39 +0800

    update README.md

--- clion_work_space/main.cpp
+++ clion_work_space/main.cpp
@@ -1,8 +1,33 @@
-#include <iostream>
+#include<stdio.h>
 
-using namespace std;
+long dir[2000],M,N,cnt = 0,left = 0,right = 0,text[2000];
 
-int main() {
-	cout << "Hello, World!" << endl;
+bool search(int fafafa){
+	for(int i = left; i < right; i++){
+		if(dir[i] == fafafa){
+			return true;
+		}
+	}
+//     not found
+	right++;
+	dir[right] = fafafa;
+	if(right - left > M){
+		left++;
+	}
+	cnt++;
+	return false;
+}
+
+int main(void){
+	for(int i = 0; i < 2000; i++){
+		dir[i] = -1;
+	}
+	scanf("%li %li",&M,&N);
+	for(int i = 0; i < N; i++){
+		scanf("%li",&text[i]);
+		search(text[i]);
+	}
+	printf("%d",cnt-1);
+//    getchar();getchar();getchar();getchar();getchar();getchar();getchar();
 	return 0;
 }
