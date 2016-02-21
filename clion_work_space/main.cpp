09f7618a15252095fc20a237a74f1bf5393e36b1
千里冰封 <zhangyinsen@126.com>
2016-02-21 22:16:53 +0800

    !

--- clion_work_space/main.cpp
+++ clion_work_space/main.cpp
@@ -1,207 +1,75 @@
 #include <stdio.h>
 #include <queue>
 using namespace std;
-
-// 长度限制
-const int size = 50;
-
-// 房间类
-typedef struct room {
-	bool north;
-	bool south;
-	bool east;
-	bool west;
-	room(int s){
-		north = false;
-		south = false;
-		east = false;
-		west = false;
-		switch (s){
-			case 1:
-				west = true;
-				break;
-			case 2:
-				north = true;
-				break;
-			case 3:
-				west = true;
-				north = true;
-				break;
-			case 4:
-				east = true;
-				break;
-			case 5:
-				west = true;
-				east = true;
-				break;
-			case 6:
-				north = true;
-				east = true;
-				break;
-			case 7:
-				west = true;
-				east = true;
-				north = true;
-				break;
-			case 8:
-				south = true;
-				break;
-			case 9:
-				west = true;
-				south = true;
-				break;
-			case 10:
-				south = true;
-				north = true;
-				break;
-			case 11:
-				west = true;
-				south = true;
-				north = true;
-				break;
-			case 12:
-				east = true;
-				south = true;
-				break;
-			case 13:
-				west = true;
-				east = true;
-				south = true;
-				break;
-			case 14:
-				east = true;
-				south = true;
-				north = true;
-				break;
-			case 15:
-				west = true;
-				east = true;
-				south = true;
-				north = true;
-				break;
-			default:
-				break;
-		}
-	}
-	room(){
-		north = false;
-		south = false;
-		east = false;
-		west = false;
-	}
-} room ;
-
-// 坐标类
+const int size = 100;
+const int go[4][2] = {
+		1, 0, -1, 0,
+        0, 1, 0, -1
+};
+char a[size][size];
+bool b[size][size];
+int x,y, cnt = 1;
 typedef struct poi{
 	int x;
 	int y;
+	poi(){x = 0; y = 0;}
+	poi(int x_, int y_){ x = x_; y = y_;}
 } poi;
 
-int main(int argc,char* argv[]){
-	room a[size][size];
-	bool b[size][size];
-	// x是横坐标, y是纵坐标
-	int y, x;
-	scanf("%i %i", &x, &y);
-	for(int i = 0; i < x; i++)
-		for(int j = 0; j < y; j++)
-			b[i][j] = false;
-
-	for(int i = 0; i < x; i++)
-		for(int j = 0; j < y; j++){
-			int s;
-			scanf("%i", &s);
-			a[i][j] = room(s);
-		}
-//	for(int i = 0; i < x; i++)
-//		for(int j = 0; j < y; j++){
-//			printf("when x = %i, y = %i, north = %i, south = %i, west = %i, east = %i\n",
-//			i, j, a[i][j].north ? 1 : 0, a[i][j].south ? 1 : 0, a[i][j].west ? 1 : 0, a[i][j].east ? 1 : 0);
-//		}
-//	return 0;
-	int max = 0;
-	int cnt = 0;
-
-	queue<poi> q;
-	while(!q.empty()) q.pop();
-
+int main(int argc, char* argv[]){
 	while(true){
-		int this_max = 1;
-
-		int loop = 0;
-		for(int i = 0; i < x; i++) {
-			for(int j = 0; j < y; j++) {
-				if(b[i][j]) loop++;
-			}
-		}
-		if(loop >= y * x) break;
-		cnt++;
-		bool loop_ = true;
-		if(q.empty()) {
-//			printf("start to find a new room.\n");
-			for(int i = 0; i < x && loop_; i++)
-				for(int j = 0; j < y; j++)
-					if(!b[i][j]) {
-						b[i][j] = true;
-						poi p;p.y = j;p.x = i;q.push(p);
-//						printf("p.x = %i, p.y = %i\n", p.x, p.y);
-						loop_ = false;
-						break;
-					}
-		}
-
+		scanf("%i%i", &y, &x);
+		if(x == 0 && y == 0)
+			break;
+		poi p;
+		queue<poi> q;
+		for(int i = 0; i < x; i++)
+			scanf("%s", a[i]);
+		for(int i = 0; i < x; i++)
+			for(int j = 0; j < y; j++)
+				if(a[i][j] == '@'){
+					p = poi(i,j);
+				}
+		while (!q.empty()) q.pop();
+//		printf("x = %i, y = %i", p.x, p.y);
+		q.push(p);
 		while(!q.empty()){
-			poi p = q.front();
+			p = q.front();
 			q.pop();
-			// 按理说不需要判断越界。因为边缘有墙。
-			if( !a[p.x][p.y].east && !b[p.x][p.y+1]){
-				p.y++;
-//				printf("p.x = %i, p.y = %i\n", p.x, p.y);
-				b[p.x][p.y] = true;
-				this_max++;
-				q.push(p);
-				p.y--;
-			}
-			if( !a[p.x][p.y].west && !b[p.x][p.y-1]){
-				p.y--;
-//				printf("p.x = %i, p.y = %i\n", p.x, p.y);
-				b[p.x][p.y] = true;
-				this_max++;
-				q.push(p);
-				p.y++;
-			}
-			if( !a[p.x][p.y].south && !b[p.x+1][p.y]){
-				p.x++;
-//				printf("p.x = %i, p.y = %i\n", p.x, p.y);
-				b[p.x][p.y] = true;
-				this_max++;
-				q.push(p);
-				p.x--;
-			}
-			if( !a[p.x][p.y].north && !b[p.x-1][p.y]){
-				p.x--;
-//				printf("p.x = %i, p.y = %i\n", p.x, p.y);
-				b[p.x][p.y] = true;
-				this_max++;
-				q.push(p);
-//				p.x++;
+			for(int i = 0; i < 4; i++){
+				p.x += go[i][0];
+				p.y += go[i][1];
+				if(p.x < 0 || p.x >= x || p.y < 0 || p.y >= y)
+					goto error233;
+				if(a[p.x][p.y] == '.' && !b[p.x][p.y]) {
+					q.push(p);
+					b[p.x][p.y] = true;
+					cnt++;
+				}
+				error233:
+				p.x -= go[i][0];
+				p.y -= go[i][1];
 			}
 		}
-
-		if(this_max > max) max = this_max;
 	}
-
-	printf("%i\n%i", cnt, max);
+	printf("%i\n", cnt);
 	return 0;
 }
 /*
- * 测试数据
-4
-7
-11 6 11 6 3 10 6
-7 9 6 13 5 15 5
-1 10 12 7 13 7 5
-13 11 10 8 10 12 13
- * 答案
-5 9
+6 9
+....#.
+.....#
+......
+......
+......
+......
+......
+#@...#
+.#..#.
+0 0
+
+2 2
+.@
+..
+0 0
  */
