8688550a77bc8024e083fddccb3e73d4b1911066
ice1000 <951394653@qq.com>
2016-03-01 16:51:56 +0800

    Update 1978.c

--- codevs/1978.c
+++ codevs/1978.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 int main(int argc, char* argv[]){
-    int last = 1;
-    int next = 1;
-    int cnt;
+    auto int last = 1;
+    auto int next = 1;
+    auto int cnt;
     scanf("%i", &cnt);
     if(cnt == 1 || cnt == 2){
         printf("1");
@@ -12,7 +12,7 @@ int main(int argc, char* argv[]){
     cnt--;
     cnt--;
     while(cnt--){
-        int i = next;
+        auto int i = next;
         next += last;
         last = i; 
     }
