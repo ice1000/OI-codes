0b0735a3ad2d01dd364afefbb0b06756051c7694
ice1000 <951394653@qq.com>
2016-03-11 19:56:37 +0800

    更新README Signed-off-by: ice1000 <NaN>

---
+++ codevs/1001.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+const int size = 1000;
+int N,M;
+int a[size][size];
+
+int main(int argc, char* argv[]){
+	scanf("%i%i", &N, &M);
+	for(auto int i = 0; i < M; i++){
+		
+	}
+	return 0;
+}
