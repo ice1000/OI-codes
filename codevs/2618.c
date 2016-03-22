9416d6e128796f59a9dd2bd67b57a8d4c1ff4a48
ice1000 <951394653@qq.com>
2016-03-22 17:03:36 +0800

    来自AC的快感:joy:!!!!

--- codevs/2618.c
+++ codevs/2618.c
@@ -3,31 +3,30 @@
 题目:p2618 核电站问题
 */
 
-// 谜之代码习惯 
-int cnt;
-int sta[0xfffff];
-int save[51][6];
-int a, b;
-
+// 谜之代码习惯，头文件写全局后面
+long long sta[0xfffff],save[51][51],a, b;
 #include <stdio.h>
 
-void dfs(int l,int t){
+long long dfs(int l,int t){
 	// if(l > b) return ;
-	if(t >= a) {
-		cnt++ ;
-		return ;
+	if(save[l][t])
+		return save[l][t];
+	if(t >= a){
+		save[l][t] = 1;
+		return 1;
 	}
-	// if(){}
 	t++;l++;
-	if(l < b)
-		dfs(l, t);
-	dfs(0, t);
+	if(l < b){
+		save[l][t] = dfs(l, t);
+		save[0][t] = dfs(0, t);
+		return save[l][t] + save[0][t];
+	}
+	save[0][t] = dfs(0, t);
+	return save[0][t];
 }
 
 int main(int argc, char* argv[]){
     scanf("%d%d", &a, &b);
-    dfs(0, 0);
-    printf("%d", cnt);
-    while(10);
+    printf("%lld", dfs(0, 0));
     return 0;
 }
