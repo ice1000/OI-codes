06b3521eb73a008dca5f1b08e9281ee31fc2dcf6
ice1000 <951394653@qq.com>
2016-04-08 19:05:35 +0800

    ~

--- POJ/ch0109/01.c
+++ POJ/ch0109/01.c
@@ -3,13 +3,13 @@
 int a[10000];
 
 int main(void){
-  int N,M;
+  int N,M,i;
   scanf("%d",&M);
-  for(int i = 0; i < M; i++){
+  for(i = 0; i < M; i++){
     scanf("%d",&a[i]);
   }
   scanf("%d",&N);
-  for(int i = 0; i < M; i++){
+  for(i = 0; i < M; i++){
     if(a[i] == N){ printf("%d",i+1);return 0;}
   }
   printf("-1");
