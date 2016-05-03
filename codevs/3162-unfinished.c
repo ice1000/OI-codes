558040d35cc507e0b74d92e08108f7fe0df01098
ice1000 <951394653@qq.com>
2016-05-03 17:47:05 +0800

    Update 3162-unfinished.c

--- codevs/3162-unfinished.c
+++ codevs/3162-unfinished.c
@@ -17,7 +17,39 @@ int main(int argc, char* argv[]){
     for(i = 1; i <= m; i++){
         for(j = i; j <= k; j++){
             for(k = i; k <= j; k++){
-                //
+                // f[i][j] = min{max(f[x][j-1], sum(x+1, i)), j<x<i}
+/*
+
+int i,j,k;
+    int mintime=INT_MIN;
+    
+    for(i = 0 ; i < n ; ++i)
+    {
+        for(j = i ; j < m ;++j)
+        {
+            for(k = i ; k <= j ; ++k)
+            {
+                sum[i][j]+=book[k];
+            }
+        }
+    }
+    for(i = 1 ; i <= n ; ++i)
+        dp[i][1] = sum[0][i-1];
+    for(j = 2 ; j <= m ; ++j)
+    {
+        for(i = j ; i <= n ; ++i)
+        {
+            dp[i][j]= INT_MAX;
+            for(k = j ; k <= i ; ++k)
+            {
+                mintime = max(dp[k][j-1],sum[k][i-1]);
+                dp[i][j] = min(dp[i][j],mintime);
+            }
+        }
+    }
+    return dp[n][m];
+
+*/
             }
         }
     }
