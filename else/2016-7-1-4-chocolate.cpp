b3662b8af3120d8d74c368339e625f9686ec6de2
狰狞的千里冰封 <ice1000kotlin@foxmail.com>
2016-07-01 15:44:07 +0800

    <_<

---
+++ else/2016-7-1-4-chocolate.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+typedef long long ll;
+
+int main(int argc, char* argv[]) {
+    ll m, n, k, f = 0, cnt = 1;
+    int i, md = 0, nd = 0, kd = 0;
+    cin>>m>>n>>k;
+    cout<<m * n * k - 1<<" ";
+    for(i = 0; ; i++) {
+//    	printf("%d \n", i);
+    	if(cnt >= m && !md)
+    		md = i ? i : 1;
+    	if(cnt >= n && !nd)
+    		nd = i ? i : 1;
+    	if(cnt >= k && !kd)
+    		kd = i ? i : 1;
+		if(md && nd && kd) break;
+    	cnt *= 2;
+	}
+	if(m > 1)
+		f += md;
+	if(n > 1)
+		f += nd;
+	if(k > 1)
+		f += kd;
+	cout<<f<<endl;
+    return 0;
+}
