b04b68be543fff2f9184686f51481a605c9438df
ice1000 <ice1000kotlin@foxmail.com>
2016-07-05 14:38:38 +0800

    commit via s script

--- clion_work_space/main.cpp
+++ clion_work_space/main.cpp
@@ -4,29 +4,17 @@
 
 #include <stdio.h>
 
-long long a[101];
-
 void init();
 
 int main(int argc, char* argv[]) {
 	init();
 	int n;
 	scanf("%i", &n);
-	while (n--) {
-		int t;
-		scanf("%i", &t);
-		a[t] ++;
-	}
-	for(n = 0; n < 100; n++) {
-		if(a[n]) {
-			printf("%i ", n);
-			a[n]--;
-		}
-	}
+
 	return 0;
 }
 
 void init() {
-	freopen("age.in", "r", stdin);
-	freopen("age.out", "w+", stdout);
+//	freopen("meal.in", "r", stdin);
+//	freopen("meal.out", "w+", stdout);
 }
