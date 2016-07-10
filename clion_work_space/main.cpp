81629daff01f7082351b678ef579eda8f219f48b
ice1000 <ice1000kotlin@foxmail.com>
2016-07-10 17:07:21 +0800

    commit via s script

--- clion_work_space/main.cpp
+++ clion_work_space/main.cpp
@@ -1,19 +1,78 @@
-/*
-作者:千里冰封
-*/
 
-#include <stdio.h>
 
-int linked_list[100];
+#include <cstdio>
+#include <algorithm>
+using namespace std;
 
-void build(int id, int l, int r) {
-	if(l >= r) return;
-	int mid = 1 + r >> 1;
-	build();
+#define lson l , m , rt << 1
+#define rson m + 1 , r , rt << 1 | 1
+#define root 1 , N , 1
+#define LL long long
+
+const int maxn = 111111;
+LL add[maxn<<2];
+LL sum[maxn<<2];
+void PushUp(int rt) {
+	sum[rt] = sum[rt<<1] + sum[rt<<1|1];
 }
-
-int main(int argc, char *argv[]) {
-	int n, m;
-	scanf("%i %i", &n, &m);
+void PushDown(int rt,int m) {
+	if (add[rt]) {
+		add[rt<<1] += add[rt];
+		add[rt<<1|1] += add[rt];
+		sum[rt<<1] += add[rt] * (m - (m >> 1));
+		sum[rt<<1|1] += add[rt] * (m >> 1);
+		add[rt] = 0;
+	}
+}
+void build(int l,int r,int rt) {
+	add[rt] = 0;
+	if (l == r) {
+		scanf("%lld",&sum[rt]);
+		return ;
+	}
+	int m = (l + r) >> 1;
+	build(lson);
+	build(rson);
+	PushUp(rt);
+}
+void update(int L,int R,int c,int l,int r,int rt) {
+	if (L <= l && r <= R) {
+		add[rt] += c;
+		sum[rt] += (LL)c * (r - l + 1);
+		return ;
+	}
+	PushDown(rt , r - l + 1);
+	int m = (l + r) >> 1;
+	if (L <= m) update(L , R , c , lson);
+	if (m < R) update(L , R , c , rson);
+	PushUp(rt);
+}
+LL query(int L,int R,int l,int r,int rt) {
+	if (L <= l && r <= R) {
+		return sum[rt];
+	}
+	PushDown(rt , r - l + 1);
+	int m = (l + r) >> 1;
+	LL ret = 0;
+	if (L <= m) ret += query(L , R , lson);
+	if (m < R) ret += query(L , R , rson);
+	return ret;
+}
+int main() {
+	int Q;
+	scanf("%d",&Q);
+	build(root);
+	while (Q --) {
+		char op[2];
+		int a , b , c;
+		scanf("%s",op);
+		if (op[0] == 'Q') {
+			scanf("%d%d",&a,&b);
+			printf("%lli\n",query(a , b ,root));
+		} else {
+			scanf("%d%d%d",&a,&b,&c);
+			update(a , b , c , root);
+		}
+	}
 	return 0;
 }
---
+++ else/2016-7-10-mouse.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+×÷Õß:Ç§Àï±ù·â
+*/
+
+int main(int argc, char *argv[]) {
+	freopen("mouse.in", "r", stdin);
+	freopen("mouse.out", "w+", stdout);
+	int n;
+	scanf("%i", &n);
+	printf("%i", n);
+	return 0;
+}
+
