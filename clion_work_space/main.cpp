fcc9b91b9f70b4d49384f046046f06157d3d5dbf
ice1000 <ice1000kotlin@foxmail.com>
2016-07-14 23:37:18 +0800

    commit via s script

--- clion_work_space/main.cpp
+++ clion_work_space/main.cpp
@@ -1,51 +1,45 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <cmath>
 
-typedef struct node {
-	char me;
-	node *next[26];
-} node;
+const int size = 110;
 
-node *root;
-
-char source[50010];
-
-void build (int index, node *this_root) {
-	if ( index >= strlen(source)) return;
-	int i, j, k;
-	for ( i = index; i < strlen(source); ++i ) {
-		if(!this_root->next[source[i] - 'a']) {
-			node *next = ( node * ) malloc(sizeof(node));
-			next->me = source[i];
-			this_root->next[source[i] - 'a'] = next;
-			build(i, next);
-		}
-	}
+int m (int x, int y) {
+	return x > y ? x : y;
 }
 
-void output(node *now) {
-	int i;
-	for ( i = 0; i < 25; ++i ) {
-		printf("%c", now->me);
-		if(now->next[i]) {
-			output(now->next[i]);
-		}
-	}
+int dp[size][size], a[size];
+
+int get (int b, int e) {
+	return abs(a[b] - a[e]) * ( e - b + 1 );
+//	return b == e ? a[e] : abs(a[b] - a[e]) * ( e - b + 1 );
 }
 
 int main (int argc, char *argv[]) {
-//	freopen("circle.in", "r", stdin);
-//	freopen("circle.out", "w+", stdout);
-	scanf("%s", source);
-	root->me = source[0];
-	build(0, root);
+//	freopen("remove.in", "r", stdin);
+//	freopen("remove.in", "w+", stdout);
+	int n = 1, i, j, k;
+	scanf("%i", &n);
+	for ( i = 0; i < n; scanf("%i", &a[i]), dp[i][i] = a[i], ++i );
+	for ( i = 0; i < n; ++i ) {
+		for ( j = 0; j < i; ++j ) {
+			int big = -1;
+			for ( k = j; k < i; k++ ) {
+				big = m(big, dp[j][k] + get(k + 1, i));
+				dp[j][i] = big;
+			}
+		}
+	}
+//	for ( int l = 0; l < n; ++l ) {
+//		for ( int i1 = 0; i1 < n; ++i1 ) {
+//			printf("%i ", dp[l][i1]);
+//		}
+//		puts("\n");
+//	}
+	printf("%i", dp[0][n - 1]);
 	return 0;
 }
 
 /*
-3
-1 2 80
-2 3 80
-3 1 20
- */
+6
+54 29 196 21 133 118
+*/
