825d7afe435eb29b16fca9e3c1c2460abd0a4597
ice1000 <ice1000kotlin@foxmail.com>
2016-07-29 20:12:31 +0800

    commit via s script

--- openjudge/ch0109/01.c
+++ openjudge/ch0109/01.c
@@ -2,16 +2,14 @@
 
 int a[10000];
 
-int main(void){
-  int N,M,i;
-  scanf("%d",&M);
-  for(i = 0; i < M; i++){
-    scanf("%d",&a[i]);
-  }
-  scanf("%d",&N);
-  for(i = 0; i < M; i++){
-    if(a[i] == N){ printf("%d",i+1);return 0;}
-  }
-  printf("-1");
-  return 0;
+int main(int argc, char* argv[]) {
+	int N, M, i;
+	scanf("%d",&M);
+	for(i = 0; i < M; i++)
+		scanf("%d",&a[i]);
+	scanf("%d", &N);
+	for(i = 0; i < M; i++)
+		if(a[i] == N) {printf("%d",++i); return 0;}
+	printf("-1");
+	return 0;
 }
