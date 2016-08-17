2f8fecf3d2ef732f0202041b429dcabea1f829a8
ice1000 <ice1000kotlin@foxmail.com>
2016-08-17 11:01:52 +0800

    commit via s script

---
+++ else/2016-8-17-C.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int bcj[100005];
+
+typedef struct {
+	int total;
+	int count;
+} jjfly ;
+
+int find(int n) {
+	if (bcj[n] == n) return n;
+	return bcj[n] = find(bcj[n]);
+}
+
+jjfly search(int father, int size) {
+	int i;
+	jjfly fly;
+	fly.total = 0;
+	fly.count = 0;
+	for (i = 1; i <= size; i++)
+		if (find(i) == father)
+			fly.total += i, fly.count++;
+	return fly;
+}
+
+void fuck(int size) {
+	int i;
+	for (i = 1; i <= size; i++) find(i);
+}
+
+void merge(int p, int q, int size) {
+	//(/O/v/O/)/
+	bcj[find(p)] = q;
+	fuck(size);
+}
+
+int main(int argc, char* argv[]) {
+	int m, n, i, j, a, b, c;
+	while (scanf("%d %d", &n, &m) != EOF) {
+		for (i = 1; i <= n; i++) bcj[i] = i;
+		while (m--) {
+			scanf("%d", &c);
+			switch (c) {
+				case 3:
+					scanf("%d", &a);
+					jjfly fly = search(bcj[a], n);
+					printf("%d %d\n", fly.count, fly.total);
+					break;
+				case 2:
+					scanf("%d %d", &a, &b);
+					bcj[a] = b;
+					fuck(n);
+					break;
+				case 1:
+					scanf("%d %d", &a, &b);
+					merge(a, b, n);
+					break;
+			}
+		}
+	}
+	return 0;
+}
