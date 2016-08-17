09ae7cc749d30905ed00c21f291c0317584ee939
ice1000 <ice1000kotlin@foxmail.com>
2016-08-17 08:56:58 +0800

    commit via s script

--- else/2016-8-17-A.c
+++ else/2016-8-17-A.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+// 苟利国家生死以，岂因祸福避趋之
+
 int main(int argc, char* argv[]) {
-	int fuck;
+	int fuck, shit = 0, suck;
 	scanf("%d", &fuck);
-	
+	while (shit * 4 <= fuck) {
+		if (!((fuck - shit * 4) % 7)) {
+			suck = (fuck - shit * 4) / 7;
+			while (shit--) printf("4");
+			while (suck--) printf("7");
+			return 0;
+		}
+		shit++;
+	}
+	printf("-1");
 	return 0;
 }
