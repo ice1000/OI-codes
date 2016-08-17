6b083992b88e8f1834ca683f03e6e038d7238ddd
ice1000 <ice1000kotlin@foxmail.com>
2016-08-17 09:45:59 +0800

    commit via s script

--- else/2016-8-17-B.c
+++ else/2016-8-17-B.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
+char shit[10005];
+long long fuck[10005];
+long long fib(int n) {
+	if (!n || n == 1) return 1;
+	if (fuck[n]) return fuck[n];
+	return fuck[n] = fib(n - 1) + fib(n - 2);
+}
 
+int main(int argc, char *argv[]) {
+	int t, damn;
+	scanf("%d", &t);
+	while (t--) {
+		scanf("%s", shit);
+		damn = strlen(shit);
+		printf("%I64d\n", fib(damn));
+	}
+	return 0;
+}
