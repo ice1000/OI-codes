7bec4e0c83888d4021477b0d85703df6d007342e
ice1000 <ice1000kotlin@foxmail.com>
2016-08-19 16:58:11 +0800

    commit via s script

--- else/2016-8-19-C.c
+++ else/2016-8-19-C.c
@@ -3,28 +3,33 @@
 
 typedef long long ll;
 
-ll n, min;
+ll n;
+ll sum = -1;
 
-ll s(ll a) {
-	ll r = 0;
-	while (a) r += a % 10, a /= 10;
+int get(ll f) {
+	int r = 0;
+	while (f) r += f % 10, f /= 10;
 	return r;
 }
 
-void find(ll l, ll r) {
-	if (l > r) return;
-	ll mid = (l + r) / 2;
-	ll fuck = mid * mid + s(mid) * mid;
-	if (fuck > n) find(l, mid);
-	else if (fuck < n) find(mid + 1, r);
-	else min = fuck;
+void shit(ll bitch, int i) {
+	if (get(bitch) != i) return ;
+	if (bitch * bitch + i * bitch != n) return ;
+	if (sum == -1) sum = bitch;
+	else if (sum > bitch) sum = bitch;
 }
 
 // x^2 + s(x) * x = n
 int main(int argc, char* argv[]) {
 	scanf("%I64d", &n);
-	find(1, sqrt(n));
-	printf("%d", min);
+	int i;
+	ll d;
+	for (i = 1; i <= 81; ++i) {
+		d = (sqrt(i * i + 4 * n) - i) / 2;
+		shit(d, i);
+		shit(++d, i);
+	}
+	printf("%I64d", sum);
 	return 0;
 }
 
