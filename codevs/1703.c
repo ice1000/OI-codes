59c1287424cdc61098ea8be301ebc71ed5a0bee1
狰狞的千里冰封 <ice1000kotlin@foxmail.com>
2016-10-03 17:36:27 +0800

    对数的妙用

---
+++ codevs/1703.c
@@ -0,0 +1,23 @@
+/*
+作者:千里冰封
+题目:p1703 指数判定
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+	int n;
+	double i, j;
+	scanf("%i", &n);
+	while (n--) {
+		scanf("%lf- %lf", &i, &j);
+		printf("%.3lf\n", (log(i) / log(j)));
+	}
+	return 0;
+}
+/*
+4 1 2 3 4
+ */
+
