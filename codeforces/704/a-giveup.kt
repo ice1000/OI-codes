a3a6d216c100cb231fc80242b85c7e813a808692
狰狞的千里冰封 <ice1000kotlin@foxmail.com>
2016-10-03 15:15:47 +0800

    看到居然有Kotlin编译器好激动

---
+++ codeforces/704/a-giveup.kt
@@ -0,0 +1,15 @@
+import java.lang.*
+import java.util.*
+
+fun main(args: Array<String>) {
+	val in = Scanner(System.`in`)
+	val n = in.nextInt()
+	val q = in.nextInt()
+	var x: Int
+	var type: Int
+	for (i in 1..q) {
+		type = in.nextInt()
+		x = in.nextInt()
+	}
+}
+
