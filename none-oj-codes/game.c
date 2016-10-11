68d9850518b5e800387c9112b8f16de718f9d8fe
master <951394653@qq.com>
2016-10-11 22:07:42 +0800

    commit via push.cmd

---
+++ none-oj-codes/game.c
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+int x[205], y[205], a[40005], b[40005];
+
+int isParallel(int i, int j) {
+	return !(a[i] * b[j] - a[j] * b[i]);
+}
+
+int main(int argc, char *argv[]) {
+	freopen("game.in", "r", stdin);
+	freopen("game.out", "w", stdout);
+	int n, i, j, k = 0, res = 0;
+	scanf("%i", &n);
+	for (i = 0; i < n; ++i) scanf("%i %i", &x[i], &y[i]);
+	for (i = 0; i < n; ++i)
+		for (j = i + 1; j < n; ++j) {
+			a[k] = x[i] - x[j];
+			b[k++] = y[j] - y[j];
+		}
+	printf("k = %i\n", k);
+	for (i = 0; i < k; ++i)
+		for (j = i + 1; j < k; ++j)
+			if (!isParallel(i, j)) ++res;
+			else printf("%i %i %i %i\n", a[i], b[i], a[j], b[j]);
+	printf("%i\n", (k * k + k) / 2 - res);
+	return 0;
+}
+/*
+4 -1 1 -2 0 0 0 1 1
+*/
