852af721e7d563b529f481688ffadea77c239eac
master <951394653@qq.com>
2016-10-13 22:07:52 +0800

    commit via push.cmd

--- vijos/1098.c
+++ vijos/1098.c
@@ -25,13 +25,13 @@ int main(int argc, char *argv[]) {
   }
   for (i = 1; i < n; ++i) {
     for (j = 0; j < i; ++j) {
-      if (a[i] > a[j] && dp1[j] + 1 > dp1[i])
+      if (a[i] > a[j])
         dp1[i] = __max(dp1[j] + 1, dp1[i]);
     }
   }
   for (i = n - 2; i >= 0; --i) {
     for (j = n - 1; j > i; --j) {
-      if (a[i] > a[j] && dp2[j] + 1 > dp2[i])
+      if (a[i] > a[j])
         dp2[i] = __max(dp2[j] + 1, dp2[i]);
     }
   }
