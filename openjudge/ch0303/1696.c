8b03cfb848a6ea64168e9cb17724f06c40d9aa7e
ice1000 <ice1000kotlin@foxmail.com>
2016-10-25 18:36:10 +0800

    commit via push.cmd

---
+++ openjudge/ch0303/1696.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char temp[100001];
+char stack[100001];
+double num_stack[100001];
+int idx, num_idx, last_idx, last[100001];
+
+int main(int argc, char *argv[]) {
+	int started = 0;
+	double value = 0;
+	while (scanf("%s", temp) != EOF) {
+		if (strlen(temp) == 1) {
+			last[last_idx++] = 666;
+			stack[idx++] = temp[0];
+		} else {
+			num_stack[num_idx++] = atof(temp);
+			while (last_idx && last[--last_idx] == 233) {
+				switch(stack[--idx]) {
+				case '+':
+					num_stack[num_idx - 2] += num_stack[num_idx - 1];
+					break;
+				case '-':
+					num_stack[num_idx - 2] -= num_stack[num_idx - 1];
+					break;
+				case '*':
+					num_stack[num_idx - 2] *= num_stack[num_idx - 1];
+					break;
+				case '/':
+					num_stack[num_idx - 2] /= num_stack[num_idx - 1];
+					break;
+				}
+				num_idx -= 1;
+//				--last_idx;
+			}
+			last[last_idx++] = 233;
+		}
+	}
+	printf("%lf", num_stack[0]);
+	return 0;
+}
+
+/*
+* + 11.0 12.0 + 24.0 35.0
++ 1 + 1 1
+*/
