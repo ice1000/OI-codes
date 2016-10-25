64bb0e0af8d9a7ae41720c16f5331387011a575b
ice1000 <ice1000kotlin@foxmail.com>
2016-10-25 21:26:27 +0800

    commit via push.cmd

---
+++ openjudge/ch0204/7909.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int a[200005];
+
+int cmp(const void *x, const void *y) {
+	return (*(int *)x) > (*(int *)y);
+}
+
+int main(int argc, const char *argv[]) {
+	int i, j, save, cnt = 0;
+	scanf("%i", &j);
+	for (i = 0; i < j; ++i) {
+		scanf("%i", &a[i]);
+	}
+	qsort(a, j, sizeof(a[0]), cmp);
+	save = a[0];
+	for (i = 0; i <= j; ++i) {
+		if (a[i] == save) ++cnt;
+		else {
+			printf("%i %i\n", save, cnt);
+			save = a[i];
+			cnt = 1;
+		}
+	}
+	return 0;
+}
+
+/*
+8
+2
+4
+2
+4
+5
+100
+2
+100
+*/
+
