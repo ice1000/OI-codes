16584e1fc34d632f2b6b629692a6494810087958
ice1000 <ice1000kotlin@foxmail.com>
2016-10-29 17:12:29 +0800

    commit via push.cmd

--- other/gdfzoj/342-bfs-反正gg了.cpp
+++ other/gdfzoj/342-bfs-反正gg了.cpp
@@ -8,24 +8,30 @@
 #endif
 #define __max(x, y) ((x) > (y) ? (x) : (y))
 #define __min(x, y) ((x) < (y) ? (x) : (y))
-#define legal(x, y) (((a[x][y]) <= (no.min + mid)) && ((a[x][y]) >= (no.max - mid)) && (!visit[x][y]))
-#define add(x, y) q.push(*new node(x, y, __min(no.min, a[x][y]), __max(no.max, a[x][y]))), ++visit[x][y];
+#define legal(x, y) \
+(((a[x][y]) <= (no.min + mid)) && \
+((a[x][y]) >= (no.max - mid)) && \
+(!visit[x][y]))
+
+#define add(x, y) \
+q.push(*new node(x, y, __min(no.min, a[x][y]), __max(no.max, a[x][y]))), \
+++visit[x][y];
 
 int n, a[101][101], visit[101][101];
 
 typedef struct node {
 	int x, y, max, min;
 
-	node(int _x = 0, int _y = 0, int _max = a[0][0], int _min = a[0][0]) :
-		x(_x), y(_y), max(_max), min(_min) { }
+	node(int _x = 1, int _y = 1, int _max = a[1][1], int _min = a[1][1]) :
+			x(_x), y(_y), max(_max), min(_min) { }
 } node;
 
 std::queue<node> q;
 
 int bs(int l, int r) {
-	if (l >= r) return r;
 	int mid = (l + r) >> 1, x, y, suc = 0;
-//	printf("<%i|%i|%i>\n", l, mid, r);
+	printf("<%i|%i|%i>\n", l, mid, r);
+	if (l >= r) return r;
 	while (!q.empty()) q.pop();
 	memset(visit, 0, sizeof(visit));
 	q.push(*new node());
@@ -37,7 +43,7 @@ int bs(int l, int r) {
 		y = no.y;
 		if (x == n && y == n) {
 			++suc;
-//			printf("[%i, %i]\n", no.max, no.min);
+			printf("[%i, %i]\n", no.max, no.min);
 			break;
 		}
 		if (x > 1 && legal(x - 1, y)) add(x - 1, y);
@@ -50,13 +56,12 @@ int bs(int l, int r) {
 }
 
 int main(int argc, const char *argv[]) {
-	freopen("C:\\Users\\ice1000\\Desktop\\GDFZOJ\\data\\road\\road8.in", "r",
-	        stdin);
+//	freopen("C:\\Users\\ice1000\\Desktop\\GDFZOJ\\data\\road\\road8.in", "r", stdin);
 	int i, j, max = -1, min = 0xfffff;
 	q = *new std::queue<node>;
 	scanf("%i", &n);
-	for (i = 0; i < n; ++i)
-		for (j = 0; j < n; ++j) {
+	for (i = 1; i <= n; ++i)
+		for (j = 1; j <= n; ++j) {
 			scanf("%i", &a[i][j]);
 			if (a[i][j] > max) max = a[i][j];
 //			if (a[i][j] < min) min = a[i][j];
