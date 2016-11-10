7cd817e4de9c6498900d7f6a512500b3c15152d8
ice1000 <ice1000kotlin@foxmail.com>
2016-11-10 19:31:26 +0800

    commit via push.cmd

---
+++ other/begin_bozj/1022.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int n;
+
+int main(int argc, const char *argv[]) {
+	puts("abcdefghijklm\n"
+			     "nopqrstuvwxyz\n"
+			     "zyxwvutsrqpon\n"
+			     "mlkjihgfedcba");
+	return 0;
+}
