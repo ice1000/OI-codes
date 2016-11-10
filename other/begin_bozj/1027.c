916052dd8f40974cd16dd8bacddaa0ab7ab1a378
ice1000 <ice1000kotlin@foxmail.com>
2016-11-10 20:27:53 +0800

    commit via push.cmd

---
+++ other/begin_bozj/1027.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int gcd(int, int);
+
+int main(int argc, const char *argv[]) {
+	int i, j;
+	scanf("%i %i", &i, &j);
+	printf("%i", gcd(i, j));
+	return 0;
+}
+
+int gcd(int i, int j) {
+	if (j > i) return gcd(j, i);
+	return !j ? i : gcd(j, i % j);
+}
