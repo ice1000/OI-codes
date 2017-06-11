8933f2d787ef05498aaf5ea7036b6cb66316523e
ice1000 <ice1000kotlin@foxmail.com>
2017-06-11 20:25:11 +0800

    add

---
+++ codewars/the-most-imperative-functional-language.hs
@@ -0,0 +1,35 @@
+module Imperative (
+  def, var, lit, while, (+=), (-=), (*=)
+  ) where
+
+data M a = M a
+data V = V Integer
+
+instance Functor M where fmap f (M a) = M $ f a
+
+instance Applicative M where
+  pure = M
+  M f <*> M a = M $ f a
+--
+
+instance Monad M where
+  return a    = M a
+  (M a) >>= f = f a
+--
+
+def :: V -> Integer
+def (V i) = i
+
+var :: Integer -> M V
+var = undefined
+
+lit :: Integer -> V
+lit = V
+
+while :: V -> (Integer -> Bool) -> M () -> M ()
+while = undefined
+
+a += b = error "todo: (+=)"
+a -= b = error "todo: (-=)"
+a *= b = error "todo: (*=)"
+
