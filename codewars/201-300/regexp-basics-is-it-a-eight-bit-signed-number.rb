c796b14ef3a5a5047e04c8a74cddf24894e0aea4
ice1000 <ice1000kotlin@foxmail.com>
2017-07-07 14:57:12 +0800

    fix errors

--- codewars/201-300/regexp-basics-is-it-a-eight-bit-signed-number.rb
+++ codewars/201-300/regexp-basics-is-it-a-eight-bit-signed-number.rb
@@ -3,6 +3,6 @@ class String
     p self
     return false if "" == self || (size > 1 && self[0] == '0') || start_with?("-0")
     return false unless /\A\-?\d+\z/ === self
-    0 == (to_i >> 8)
+    [0, -1].include?(to_i >> 7)
   end
 end
--- codewars/201-300/regexp-basics-is-it-a-eight-bit-unsigned-number.rb
+++ codewars/201-300/regexp-basics-is-it-a-eight-bit-unsigned-number.rb
@@ -1,5 +1,5 @@
 class String
-  def signed_eight_bit_number?
+  def eight_bit_number?
     p self
     return false if "" == self || (size > 1 && self[0] == '0') || start_with?("-0")
     return false unless /\A\-?\d+\z/ === self
