9f872f590271cc6220eb911239eeafcecdda9ebe
ice1000 <ice1000kotlin@foxmail.com>
2017-07-11 23:58:17 +0800

    this is the first hs-only kata

--- codewars/301-400/lensmaker.hs
+++ codewars/301-400/lensmaker.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE TupleSections #-}
 
 module MicroLens where
 
@@ -66,11 +67,6 @@ type Optic p f s t a b =
   p a (f b) -> p s (f t)
 --
 
-type Iso s t a b =
-  forall p f . (Profunctor p, Functor f) =>
-  Optic p f s t a b
---
-
 type Lens s t a b =
   forall f . Functor f => 
   Optic (->) f s t a b
@@ -91,6 +87,11 @@ type Prism s t a b =
   Optic p f s t a b
 --
 
+type Iso s t a b =
+  forall p f . (Profunctor p, Functor f) =>
+  Optic p f s t a b
+--
+
 ---------------------------------------------------------
 ---------------------------------------------------------
 -- Todo
@@ -99,11 +100,11 @@ type Prism s t a b =
 -- f :: a -> Functor b
 -- g :: (a, x) -> Functor (b, x)
 _1 :: Lens (a, x) (b, x) a b
-_1 = \f -> \(a, x) -> (\b -> (b, x)) <$> f a
+_1 f (a, x) = (, x) <$> f a
 
 -- | A lens focusing on the second element in a pair
 _2 :: Lens (x, a) (x, b) a b
-_2 = \f -> \(x, a) -> (\b -> (x, b)) <$> f a
+_2 f (x, a) = (x, ) <$> f a
 
 -- | A function which takes a lens and looks through it.
 -- The type given is specialized to provide a hint as to
@@ -126,7 +127,7 @@ view f = getK . f K
 -- over :: Lens s t a b -> (a -> b) -> (s -> t)
 -- @_@
 over :: Optic (->) Id s t a b -> (a -> b) -> (s -> t)
-over = undefined
+over l f = getId . l (Id . f)
 
 -- | A function from a lens and a value which sets the value
 -- at the focal point of the lens. The type given has been
@@ -138,7 +139,8 @@ over = undefined
 -- @_@
 -- >_> (a -> Id b) -> (s -> Id t)
 set :: Optic (->) Id s t a b -> b -> (s -> t)
-set f b = getId . f (const $ Id b)
+--set f b = getId . f (const $ Id b)
+set f = over f . const
 
 -- | A traversal which focuses on each element in any 
 -- Traversable container.
@@ -155,7 +157,7 @@ elements = traverse
 -- @_@
 -- >_> (a -> f a) -> (s -> f s)
 toListOf :: Optic (->) (K (Endo [a])) s s a a -> (s -> [a])
-toListOf f = (`appEndo` []) . getK  . f pure
+toListOf f = (`appEndo` []) . getK  . f (K . Endo . (:))
 
 -- | A function which takes any kind of Optic which might
 -- be focused on zero subparts and returns Just the first
@@ -165,29 +167,33 @@ toListOf f = (`appEndo` []) . getK  . f pure
 -- preview :: Traversal s s a a -> (s -> Maybe a)
 -- @_@
 preview :: Optic (->) (K (First a)) s s a a -> (s -> Maybe a)
-preview f = getFirst . getK . f pure
+preview f = getFirst . getK . f (K . First . Just)
 
 -- | A helper function which witnesses the fact that any
 -- container which is both a Functor and a Contravariant
 -- must actually be empty.
 coerce :: (Contravariant f, Functor f) => f a -> f b
-coerce = undefined
+coerce = contramap (const ()) . (const () <$>)
 
 -- | A Fold which views the result of a function application
 -- contramap f :: (f b -> f a)
 -- return type: (b -> f b) -> (a -> f a)
 to :: (a -> b) -> Fold a b
-to f = \g -> contramap f . g . f
+to f g = coerce . g . f
 
 -- | A prism which focuses on the left branch of an Either
 _Left :: Prism (Either a x) (Either b x) a b
-_Left = error "todo"
+_Left = rmap (either (Left <$>) $ pure . Right) . left'
   
 -- | A prism which focuses on the right branch of an Either
 _Right :: Prism (Either x a) (Either x b) a b
-_Right = error "todo"
+_Right = rmap (either (pure . Left) (Right <$>)) . right'
 
 -- | An iso which witnesses that tuples can be flipped without
 -- losing any information
+-- dimap :: (a1 -> a) -> (b -> b1) -> (a -> b) -> (a1 -> b1)
+-- >_> (ba -> f ba) ->> (ab -> f ab)
 _flip :: Iso (a, b) (a, b) (b, a) (b, a)
-_flip = error "todo"
+_flip = dimap flipT (flipT <$>)
+  where flipT (a, b) = (b, a)
+--
